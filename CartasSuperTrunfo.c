dc5336fc61a43f864f020a5489ed8a53a129ebb8
Marcozzs <candeiamarcos8@gmail.com>
2025-04-28 22:21:54 -0300

    Update and rename Cartas Super Trunfo - Iniciante.c to CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,8 @@ int main(){
     float area_em_km1;
     float pib1;
     int numero_de_pontos_turisticos1;
+    float densidade_populacional1;
+    
 
     //CARTA 2:
     char estado2[20];
@@ -19,6 +21,7 @@ int main(){
     float area_em_km2;
     float pib2;
     int numero_de_pontos_turisticos2;
+    
 
     //CARTA 1:
     printf("Vamos cadastrar a primeira carta!\n");
@@ -28,7 +31,7 @@ int main(){
 
     printf("Qual o código: ", codigo_da_carta1);
     scanf("%s", &codigo_da_carta1);
-    getchar(); //Limpeza do buffer, para rodar normalmente o próximo fgets
+    getchar();
 
     printf("Qual o nome da cidade: ", nome_da_cidade1);
     fgets(nome_da_cidade1, 50, stdin);
@@ -44,7 +47,13 @@ int main(){
 
     printf("Qual o número de Pontos Turísiticos: ", numero_de_pontos_turisticos1);
     scanf("%d", &numero_de_pontos_turisticos1);
-    getchar(); //Limpeza do buffer, para rodar normalmente o próximo fgets
+    getchar();
+
+    //OPERACÃO CARTA1 PÓS COLETA;
+    float densidade_populacional1 = (float) populacao1/area_em_km1;
+    float pib_per_capita1 = (float) (pib1/populacao1) *1000000000;//Foi necessário converter o PIB de bilhões para reais para calcular o PIB per capita.
+    
+
 
     //CARTA 2:
     printf("\nVamos cadastrar a segunda carta!\n");
@@ -54,7 +63,7 @@ int main(){
 
     printf("Qual o código: ", codigo_da_carta2);
     scanf("%s", &codigo_da_carta2);
-    getchar(); //Limpeza do buffer, para rodar normalmente o próximo fgets
+    getchar();
 
     printf("Qual o nome da cidade: ", nome_da_cidade2);
     fgets(nome_da_cidade2, 20, stdin);
@@ -70,28 +79,35 @@ int main(){
 
     printf("Qual o número de Pontos Turísiticos: ", numero_de_pontos_turisticos2);
     scanf("%d", &numero_de_pontos_turisticos2);
-    getchar(); //Limpeza do buffer, para rodar normalmente o próximo fgets
+    getchar();
 
-    //RESULTADO CARTA 1:
+    //OPERACÃO CARTA2 PÓS COLETA;
+    float densidade_populacional2 = (float) populacao2/area_em_km2;
+    float pib_per_capita2 = (float) (pib2/populacao2) *1000000000;//Foi necessário converter o PIB de bilhões para reais para calcular o PIB per capita.
+    
+
+    //RESULTADO FINAL:
     printf("\nCarta 1:\n");
     printf("Estado:%s", estado1);
     printf("Código:%s\n", codigo_da_carta1);
     printf("Nome da Cidade:%s", nome_da_cidade1);
     printf("Populacão:%d\n", populacao1);
-    printf("Área: %f\n", area_em_km1);
-    printf("PIB:%f\n", pib1);
+    printf("Área:%.2f\n", area_em_km1);
+    printf("PIB:%.2f\n", pib1);
     printf("Número de Pontos Turísticos:%d\n", numero_de_pontos_turisticos1);
+    printf("Densidade Populacional:%.2f\n", densidade_populacional1);
+    printf("PIB per Capita:%.2f\n", pib_per_capita1);
 
-    //RESULTADO CARTA 2:
     printf("\nCarta 2:\n");
     printf("Estado:%s", estado2);
     printf("Código:%s\n", codigo_da_carta2);
     printf("Nome da Cidade:%s", nome_da_cidade2);
     printf("Populacão:%d\n", populacao2);
-    printf("Área: %f\n", area_em_km2);
-    printf("PIB:%f\n", pib2);
+    printf("Área: %.2f\n", area_em_km2);
+    printf("PIB:%.2f\n", pib2);
     printf("Número de Pontos Turísticos:%d\n", numero_de_pontos_turisticos2);
-
-
+    printf("Densidade Populacional: %.2f\n", densidade_populacional2);
+    printf("PIB per Capita:%.2f\n", pib_per_capita2);
+    
     return 0;
 }
